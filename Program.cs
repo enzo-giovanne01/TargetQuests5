cbec4a07691834eae815b074ccb1fa60b4493d70
enzo-giovanne01 <giovanneenzo01@gmail.com>
2024-09-03 18:31:12 -0300

    targetQuests

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+class InversorDeString
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Digite uma string: ");
+        string entrada = Console.ReadLine();
+
+        char[] arrayDeCaracteres = entrada.ToCharArray();
+        int esquerda = 0;
+        int direita = arrayDeCaracteres.Length - 1;
+
+        while (esquerda < direita)
+        {
+            char temp = arrayDeCaracteres[esquerda];
+            arrayDeCaracteres[esquerda] = arrayDeCaracteres[direita];
+            arrayDeCaracteres[direita] = temp;
+            esquerda++;
+            direita--;
+        }
+
+        string invertida = new string(arrayDeCaracteres);
+
+        Console.WriteLine("String invertida: " + invertida);
+    }
+}
---
+++ README.md
@@ -0,0 +1,1 @@
+# TargetQuests5
